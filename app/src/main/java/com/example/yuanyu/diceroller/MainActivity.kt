586ea20fb1b5c18a05c62da909041ceb74a5dd4f
yuanyu90221 <yuanyu90221@gmail.com>
2019-05-26 21:44:52 +0800

    update with with lateinit with diceImage

--- app/src/main/java/com/example/yuanyu/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/yuanyu/diceroller/MainActivity.kt
@@ -10,6 +10,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +19,11 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_img)
     }
 
     private fun rollDice() {
-
         val randomInt = Random().nextInt(6)+1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
@@ -30,7 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         };
-        val diceImage: ImageView = findViewById(R.id.dice_img)
         diceImage.setImageResource(drawableResource)
     }
 }
